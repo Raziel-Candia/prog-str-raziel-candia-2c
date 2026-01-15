e0e11cea49739b4ab3cd7028d4e5c0f6bb228b19
Raziel Candia <20253ds068@utez.edu.mx>
2026-01-15 07:55:51 -0600

    chore: base actividad git merge withoutconflict

--- MergeWithOutConflict/src/Main.java
+++ MergeWithOutConflict/src/Main.java
@@ -4,8 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("semana 2");
         System.out.println("Versión base");
-        System.out.println("2+2=4");
-        System.out.println("5*3=15");
 
 
     }
